d29348aa07f6cc69c3c8e90c557375d17c78f9c1
jankikhatri <jankikhatri72@gmail.com>
2020-05-10 18:52:21 -0400

    Homework

--- Tests/test_MathOperations.py
+++ Tests/test_MathOperations.py
@@ -11,7 +11,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(3, Addition.sum(1, 2))
 
     def test_MO_subtract(self):
-        self.assertEqual((-1, Subtraction.subtract(1, 2)))
+        self.assertEqual(-1, Subtraction.subtract(1, 2))
 
     def test_MO_multiply(self):
         self.assertEqual(2, Multiplication.multiply(1, 2))
